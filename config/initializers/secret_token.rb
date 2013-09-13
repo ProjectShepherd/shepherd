380534c30fe650a82f6d9e81376ad001adadb866
Marc Chung <marc@lookout.com>
2013-09-13 16:36:36 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProjectShepherd::Application.config.secret_key_base = 'd88b97c795cc22abcd9654e2c7d25836ef96227883df96d20a80963b12e2d838171d51c373eea2892bc455e7856c82b26185cedd385dececfbb800e94aa28c5b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ProjectShepherd::Application.config.session_store :cookie_store, key: '_ProjectShepherd_session'
